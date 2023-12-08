c7132eb1c351136521b7563a4a00bcb0eb123fa2
MuaazH <muaaz.h.is@gmail.com>
2023-12-08 19:58:04 +0100

    enable debug symbols in test compiler

--- compile.cmd
+++ compile.cmd
@@ -1,3 +1,8 @@
 @echo off
 cls
-g++ -Wall -I..\mlib ..\x86assm32\x86_assm.cpp -DSYSTEM_C_NO_NETWORK ..\mlib\system.c main.cpp -static-libgcc -static-libstdc++ -o loader.exe
+g++ -c -m32 -Wall ..\c_lib\memory.c                              -o obj\memory.o
+g++ -c -m32 -Wall ..\c_lib\c_strings.c                           -o obj\c_strings.o
+g++ -c -m32 -Wall ..\c_lib\io.c                                  -o obj\io.o
+g++ -c -m32 -Wall -I..\mlib -I..\c_lib ..\x86assm32\x86_assm.cpp -o obj\x86_asm.o
+g++ -c -m32 -Wall -I..\mlib -I..\c_lib main.cpp                  -o obj\main.o
+g++ -m32 -static-libgcc -static-libstdc++ obj\main.o obj\memory.o obj\c_strings.o obj\io.o obj\x86_asm.o -o loader.exe
